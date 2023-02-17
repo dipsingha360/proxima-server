e108727bf6d5a5ae77af316466d02e24db937ca2
Dip Singha <dipsingha2@gmail.com>
2023-02-17 17:27:10 +0600

    cors added on server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
 require("dotenv").config();
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
