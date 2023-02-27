faad3a67b3a1c0d3c143c7c4db81dcc6e640cd70
Dip Singha <dipsingha2@gmail.com>
2023-02-28 01:12:40 +0600

    userRoute, userController, userModel created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
+const userRoutes = require("./routes/userRoute");
 require("dotenv").config();
 
 // experess app
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/projects", projectRoutes);
+app.use("/api/user", userRoutes);
 
 // mongodb
 mongoose.set("strictQuery", false);
