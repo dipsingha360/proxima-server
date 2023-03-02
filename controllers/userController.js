8530d76b1ddf11f26e2989c5eb2b2ea8f48b1d3f
Dip Singha <dipsingha2@gmail.com>
2023-03-02 23:41:41 +0600

    JWT added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// create token function
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "10d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,7 +17,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json(user);
+    // crate token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
